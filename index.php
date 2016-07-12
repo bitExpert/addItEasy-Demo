74ef064cfabbfd87fb064ee0bb11d7a4b62b4f4e
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2016-07-12 10:01:50 +0200

    Fixed vendor path issues.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . "/vendor/autoload.php"
-require __DIR__ . "/config/config.inc.php"
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/config/config.inc.php";
 // bootstrap and run addITeasy
-require __DIR__ . "/vendor/bitexpert/addITeasy/src/bootstrap.php"
+require __DIR__ . "/vendor/bitexpert/additeasy/src/bootstrap.php";
